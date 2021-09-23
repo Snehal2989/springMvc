7346e228dd34999944fcb695c935707c1b622e2c
Snehal2989 <85414199+Snehal2989@users.noreply.github.com>
2021-09-23 17:51:06 +0530

    Add files via upload

---
+++ SpringBoot_JPA_ex2/src/main/java/com/cjc/SpringBootJpaEx2Application.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaEx2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaEx2Application.class, args);
+	}
+
+}
